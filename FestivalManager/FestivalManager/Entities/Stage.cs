6f55a6182aaf54185f805f48e4f98d7d5c7c39ba
Daniel N <DaniGwen@users.noreply.github.com>
2020-04-14 21:06:26 +0300

    Update

--- FestivalManager/FestivalManager/Entities/Stage.cs
+++ FestivalManager/FestivalManager/Entities/Stage.cs
@@ -1,68 +1,75 @@
 ﻿namespace FestivalManager.Entities
 {
-	using System.Collections.Generic;
-	using Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
 
-	public class Stage : IStage
-	{
-		private readonly List<ISet> sets;
-		private readonly List<ISong> songs;
-		private readonly List<IPerformer> performers;
+    public class Stage : IStage
+    {
+        private readonly List<ISet> sets;
+        private readonly List<ISong> songs;
+        private readonly List<IPerformer> performers;
 
-		protected Stage()
-		{
-			this.sets = new List<ISet>();
-			this.songs = new List<ISong>();
-			this.performers = new List<IPerformer>();
-		}
+        protected Stage()
+        {
+            this.sets = new List<ISet>();
+            this.songs = new List<ISong>();
+            this.performers = new List<IPerformer>();
+        }
 
-		public IReadOnlyCollection<ISet> Sets => this.sets.AsReadOnly();
-		public IReadOnlyCollection<ISong> Songs => this.songs.AsReadOnly();
-		public IReadOnlyCollection<IPerformer> Performers => this.performers.AsReadOnly();
+        public IReadOnlyCollection<ISet> Sets => this.sets.AsReadOnly();
+        public IReadOnlyCollection<ISong> Songs => this.songs.AsReadOnly();
+        public IReadOnlyCollection<IPerformer> Performers => this.performers.AsReadOnly();
 
-		public void AddPerformer(IPerformer performer)
-		{
-			throw new System.NotImplementedException();
-		}
+        public void AddPerformer(IPerformer performer)
+        {
+            if (performer != null)
+            {
+                this.performers.Add(performer);
+            }
+            else
+            {
+                throw new ArgumentNullException("Performer can not be null!");
+            }
+        }
 
-		public void AddSet(ISet performer)
-		{
-			throw new System.NotImplementedException();
-		}
+        public void AddSet(ISet set)
+        {
+            if (set == null)
+            {
+                throw new ArgumentNullException("Set can not be null!");
+            }
 
-		public void AddSong(ISong song)
-		{
-			throw new System.NotImplementedException();
-		}
+            this.sets.Add(set);
+        }
 
-		public IPerformer GetPerformer(string name)
-		{
-			throw new System.NotImplementedException();
-		}
+        public void AddSong(ISong song)
+        {
+            if (song == null)
+            {
+                throw new ArgumentNullException("Song can not be null!");
+            }
 
-		public ISet GetSet(string name)
-		{
-			throw new System.NotImplementedException();
-		}
+            this.songs.Add(song);
+        }
 
-		public ISong GetSong(string name)
-		{
-			throw new System.NotImplementedException();
-		}
+        public IPerformer GetPerformer(string name)
+                     => this.performers.FirstOrDefault(p => p.Name == name);
 
-		public bool HasPerformer(string name)
-		{
-			throw new System.NotImplementedException();
-		}
+        public ISet GetSet(string name)
+                     => this.sets.FirstOrDefault(s => s.Name == name);
 
-		public bool HasSet(string name)
-		{
-			throw new System.NotImplementedException();
-		}
+        public ISong GetSong(string name)
+                     => this.songs.FirstOrDefault(s => s.Name == name);
 
-		public bool HasSong(string name)
-		{
-			throw new System.NotImplementedException();
-		}
-	}
+        public bool HasPerformer(string name)
+                     => this.performers.Any(p => p.Name == name);
+
+        public bool HasSet(string name)
+                     => this.sets.Any(s => s.Name == name);
+
+        public bool HasSong(string name)
+                     => this.songs.Any(s => s.Name == name);
+    }
 }
